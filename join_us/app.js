a078d40bf2adc3d7730c54035e70340db8e172d6
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-10-10 18:10:16 -0700

    MySQL course app

--- join_us/app.js
+++ join_us/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true}))
 
 // dotenv config
 require('dotenv/config')
@@ -15,15 +19,26 @@ const connection = mysql.createConnection({
 })
 
 app.get("/", (req, res) => {
- const q = 'SELECT COUNT(*) as count FROM users';
- connection.query(q, (error, results) => {
- if (error) throw error;
- const msg = "We have " + results[0].count + " users";
- res.send(msg);
- });
+    const q = 'SELECT COUNT(*) as count FROM users';
+    connection.query(q, (error, results) => {
+        if (error) throw error;
+            const count = results[0].count
+            res.render('home',{data: count});
+    });
 });
 
+app.post("/register", (req, res) => {
+    const email = req.body.email
+    const person = {
+      email: req.body.email
+    }
+    connection.query("INSERT INTO users SET ?", person,
+      (err, result) => {
+        if (err) throw err;
+        res.redirect("/")
+      })
+    console.log("Post sent to register, email is " + req.body.email)
+})
 app.listen(8080, () => {
     console.log('app listening on port 8080')
 })
-
